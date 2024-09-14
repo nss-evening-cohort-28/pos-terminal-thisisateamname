8bba665c1fb27438b20898a379b9489698ab974d
MaxJones129 <maxjones129@gmail.com>
2024-09-14 11:46:36 -0500

    built forms, they currently have no function

--- components/forms/addPaymentForm.js
+++ components/forms/addPaymentForm.js
@@ -1,11 +1,25 @@
 import clearDom from '../../utils/clearDom';
 import renderToDOM from '../../utils/renderToDom';
 
-const addPaymentForm = (/* obj = {} */) => {
+const addPaymentForm = (obj = {}) => {
   clearDom();
   const domString = `
-  
-  `;
+  <form id="${obj.firebaseKey ? `update-payment--${obj.firebaseKey}` : 'submit-payment'}" class="mb-4">
+      <div class="form-group">
+        <label for="title">Select a Payment Type</label>
+        <select class="form-control" id="payment">
+          <option value="cash">Cash</option>
+          <option value="credit">Credit</option>
+          <option value="mobile">Mobile</option>
+      </div>
+      <div class="form-group">
+        <label for="title">Tip Amount</label>
+        <input type="text" class="form-control" id="tip" placeholder="Ex. 10.00" value="${obj.tip || ''}" required>
+      </div>
+      <div class="d-grid gap-2 col-6 mx-auto">
+        <button type="submit" class="btn btn-success mt-3">Close Order</button>
+      </div>
+    </form>`;
 
   renderToDOM('#form-container', domString);
 };
--- pages/homePage.js
+++ pages/homePage.js
@@ -4,7 +4,7 @@ import renderToDOM from '../utils/renderToDom';
 const homePage = () => {
   clearDom();
   const domString = `
-    <h5 class="homePage-title">Welcome User!</h5>
+    <h3 class="homePage-title">Welcome User!</h3>
     <div class="d-grid gap-2 col-6 mx-auto">
       <button class="btn btn-success" type="button" id="viewOrderBtn">View Orders</button>
       <button class="btn btn-info" type="button" id="createOrderBtn">Create an Order</button>
