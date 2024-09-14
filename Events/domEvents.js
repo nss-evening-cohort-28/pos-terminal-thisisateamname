ea740ff547d9ae8f40698970b77f869c3dd9d13f
sirenabailie <sirenabailie@gmail.com>
2024-09-14 12:32:17 -0500

    view orders nav button functioning. TODO: dom view order button

---
+++ Events/domEvents.js
@@ -0,0 +1,11 @@
+// import getOrders from '../api/orderData';
+// import { showOrders } from '../pages/orderCard';
+
+// const domEvents = () => {
+//   // click event to view orders
+//   document.querySelector('#viewOrderBtn').addEventListener('click', () => {
+//     getOrders().then(showOrders);
+//   });
+// };
+
+// export default domEvents;
