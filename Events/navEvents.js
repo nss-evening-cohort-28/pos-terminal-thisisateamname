1e10de745de3d256259e0ef95db2cda671ee6c02
David Trejo LT <trejo.m.david98@gmail.com>
2024-09-16 19:32:04 -0500

    added home button

--- Events/navEvents.js
+++ Events/navEvents.js
@@ -1,9 +1,13 @@
 import getOrders from '../api/orderData';
+import homePage from '../pages/homePage';
 import { showOrders } from '../pages/orderCard';
 
 const navigationEvents = () => {
   document.querySelector('#allOrders').addEventListener('click', () => {
     getOrders().then(showOrders);
+  });
+  document.querySelector('#homeBtn').addEventListener('click', () => {
+    homePage();
   });
 };
 export default navigationEvents;
