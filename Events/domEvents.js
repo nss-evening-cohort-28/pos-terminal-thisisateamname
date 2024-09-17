00314373677311a37cc624f415dfbae2db8ffb44
Bayez77 <mrbayez88@gmail.com>
2024-09-16 19:33:55 -0500

    items event and getItems promise

--- Events/domEvents.js
+++ Events/domEvents.js
@@ -1,11 +1,20 @@
+import getItems from '../api/itemData';
 import getOrders from '../api/orderData';
 import { showOrders } from '../pages/orderCard';
+import { showItems } from '../pages/itemCard';
 
 const domEvents = () => {
   // click event to view orders
   document.querySelector('#viewOrderBtn').addEventListener('click', (e) => {
     if (e.target.id.includes('viewOrderBtn')) {
       getOrders().then(showOrders);
+    }
+  });
+
+  //   Event for itemsDetail button
+  document.querySelector('#itemDetailsBtn').addEventListener('click', (e) => {
+    if (e.target.id.includes('itemDetailsBtn')) {
+      getItems().then(showItems);
     }
   });
 };
--- api/itemData.js
+++ api/itemData.js
@@ -1,8 +1,19 @@
-// import client from '../utils/client';
+import client from '../utils/client';
 
-// const endpoint = client.databaseURL;
+const endpoint = client.databaseURL;
 
 // TODO: PROMISE FOR GET ITEMS
+const getItems = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/items.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
 
 // TODO: PROMISE FOR CREATE ITEMS
 
@@ -11,3 +22,5 @@
 // TODO: PROMISE FOR GET SINGLE ITEM
 
 // TODO: PROMISE FOR EDIT ITEMS
+
+export default getItems;
