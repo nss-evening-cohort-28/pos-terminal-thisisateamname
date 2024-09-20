5f8013c90242025ccaf422e2dcf88b4905cedc9a
David Trejo LT <trejo.m.david98@gmail.com>
2024-09-19 19:25:32 -0500

    revenue page now functioning

--- Events/domEvents.js
+++ Events/domEvents.js
@@ -3,18 +3,21 @@ import { showOrders } from '../pages/orderCard';
 import addOrderForm from '../components/forms/addOrderForm';
 import { getItems } from '../api/itemData';
 import { showItems } from '../pages/itemCard';
+import viewRevenue from '../pages/revenue';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     if (e.target.id.includes('viewOrderBtn')) {
       getOrders().then(showOrders);
     }
-
     if (e.target.id.includes('createOrderBtn')) {
       addOrderForm();
     }
     if (e.target.id.includes('itemDetailsBtn')) {
       getItems().then(showItems);
+    }
+    if (e.target.id.includes('viewRevenueBtn')) {
+      getItems().then(viewRevenue);
     }
   });
 };
